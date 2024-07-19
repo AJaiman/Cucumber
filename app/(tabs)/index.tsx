d9e797b6e8ff7ba6bdf93054c8146541ccceb57d
AJaiman <aravjaiman@outlook.com>
2024-07-18 23:20:35 -0400

    You can see what you type now and you can tap out of text inputs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import Button from '@/components/Button';
 import Court from '@/components/Court';
 import { ThemedText } from '@/components/ThemedText';
 import { useState } from 'react';
-import { StyleSheet, Text, SafeAreaView, View, ViewStyle, StyleProp } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, ViewStyle, StyleProp, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { GestureHandlerRootView, TextInput } from 'react-native-gesture-handler';
 
@@ -82,14 +82,10 @@ export default function HomeScreen() {
 
 
   return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <View style={styles.mainContainer}>
       <ThemedText type='title' style={{fontWeight: 900, fontSize: 40}}>Cucumber</ThemedText>
       <ThemedText type='title' style={{color: '#AADB1E', fontWeight:800, marginTop:40}}>Report Scores</ThemedText>
-      <ThemedText>Type of Game</ThemedText>
-      <View style={styles.gameSelectView}>
-        <Button isSelected={isSingles} onPress={onPressSingles} label='Singles' />
-        <Button isSelected={!isSingles} onPress={onPressDoubles} style={{marginLeft: 20}} label='Doubles' />
-      </View>
       <ThemedText style={{marginTop: 10}}>Game Info</ThemedText>
       <View style={{display: 'flex', flexDirection:'row', marginLeft: 20, marginVertical: 10}}>
         <ScoreInput color='255, 45, 45'/>
@@ -110,7 +106,13 @@ export default function HomeScreen() {
           </>)
         }
       </ Court>
+      <ThemedText style={{marginTop: 20}}>Type of Game</ThemedText>
+      <View style={styles.gameSelectView}>
+        <Button isSelected={isSingles} onPress={onPressSingles} label='Singles' />
+        <Button isSelected={!isSingles} onPress={onPressDoubles} style={{marginLeft: 20}} label='Doubles' />
+      </View>
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
